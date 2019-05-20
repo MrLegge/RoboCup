56b7ce8d8b5c76fb99db2ca155f4dff5debd2890
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2019-05-21 08:41:50 +1000

    Add files via upload

---
+++ laser/RobotLaser.cpp
@@ -0,0 +1,22 @@
+#include "RobotLaser.h"
+#include <Arduino.h>
+
+
+RobotLaser::RobotLaser(void){
+	sensorValue = 0;
+	sensorPin = A0;
+	ledPin = 13;
+	pinMode(ledPin, OUTPUT);
+	}
+    
+	void RobotLaser::Initialize(){    
+		digitalWrite(ledPin, HIGH);
+	}
+	bool RobotLaser::beamBroken(){
+		sensorValue = analogRead(sensorPin);
+		if (sensorValue < 850){
+			return true;
+		}else{
+			return false;
+		}
+	}
---
+++ laser/laser.ino
@@ -0,0 +1,15 @@
+#include "RobotLaser.h"
+RobotLaser *myLaser;
+
+void setup() {
+myLaser = new RobotLaser();
+myLaser->Initialize();
+Serial.begin(9600);
+}
+
+void loop() {
+	if (myLaser->beamBroken())
+		Serial.println("get out of my light!");
+	else
+		Serial.println("that's better!");
+}

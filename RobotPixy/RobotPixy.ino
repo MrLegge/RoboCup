0bb0ee4bc256a7976633ab1d1d40d0e8b1fadd3e
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2019-05-21 08:43:03 +1000

    Add files via upload

---
+++ RobotPixy/RobotPixy.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>  
+#include <Pixy.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.print("Starting...\n");
+  pixy.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  static int i = 0;
+  int j;
+  uint16_t blocks;
+  char buf[32]; 
+  
+  // grab blocks!
+  blocks = pixy.getBlocks();
+  
+  // If there are detect blocks, print them!
+  if (blocks)
+  {
+    i++;
+    
+    // do this (print) every 50 frames because printing every
+    // frame would bog down the Arduino
+    if (i%50==0)
+    {
+      sprintf(buf, "Detected %d:\n", blocks);
+      Serial.print(buf);
+      for (j=0; j<blocks; j++)
+      {
+        sprintf(buf, "  block %d: ", j);
+        Serial.print(buf); 
+        pixy.blocks[j].print();
+        sprintf(buf, "Area : %d", pixy.blocks[j].width*pixy.blocks[j].height);
+        Serial.println(buf);
+      }
+    }
+  } 
+}
+
+

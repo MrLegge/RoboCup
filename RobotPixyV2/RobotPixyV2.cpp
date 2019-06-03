af6c0436d7edaa77a0918b6b6d81190c21e5fce3
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2019-06-04 08:58:20 +1000

    Add files via upload

---
+++ RobotPixyV2/RobotPixyV2.cpp
@@ -0,0 +1,76 @@
+#include"RobotPixyV2.h"
+
+
+
+RobotPixyV2::RobotPixyV2(void) {
+  myPixy.init();
+ }
+
+void RobotPixyV2::getData(){  
+  blocks = myPixy.ccc.getBlocks();  
+ } 
+
+int RobotPixyV2::getArea(int requiredSignature){
+   getData();
+   int temp =0;
+	if (blocks){    
+      for (int j=0; j<blocks; j++){
+		if (myPixy.ccc.blocks[j].signature == requiredSignature){
+      for (int i = 0;i<10;i++){		
+			 temp = temp + myPixy.ccc.blocks[j].width*myPixy.ccc.blocks[j].height;
+      }
+      return temp/10;
+		}
+		else
+			return 0;
+		}
+	}else{
+			return -1;
+		}	
+}
+
+int RobotPixyV2::getX(int requiredSignature){
+  getData();
+	if (blocks){      
+      for (int j=0; j<blocks; j++){ 
+		if (myPixy.ccc.blocks[j].signature == requiredSignature)
+			return myPixy.ccc.blocks[j].x;       
+		}
+		}else{
+			return -1;
+		}	
+	}
+	
+int RobotPixyV2::getY(int requiredSignature){
+  getData();
+	if (blocks){      
+      for (int j=0; j<blocks; j++){ 
+		if (myPixy.ccc.blocks[j].signature == requiredSignature)
+			return myPixy.ccc.blocks[j].y;       
+		}
+		}else{
+			return -1;
+		}	
+	}
+
+bool RobotPixyV2::isInFront(int requiredSignature){
+   getData();
+	if (blocks){      
+      for (int j=0; j<blocks; j++){  
+		if(myPixy.ccc.blocks[j].x > 120 && myPixy.ccc.blocks[j].x < 180 && myPixy.ccc.blocks[j].signature == requiredSignature){
+			return true;      
+		}else{
+			return false;
+			}
+	  }		
+	}else {
+		return -1;
+	}
+}
+
+void RobotPixyV2::lightsOn(){
+	myPixy.setLamp(true,true);	
+}
+void RobotPixyV2::lightsOff(){
+	myPixy.setLamp(false,false);	
+}
---
+++ RobotPixyV2/RobotPixyV2.ino
@@ -0,0 +1,27 @@
+ #include"RobotPixyV2.h"
+ #include <Arduino.h>
+ 
+ RobotPixyV2 *pixy;
+
+void setup() {
+    pixy = new RobotPixyV2();
+	Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+	pixy->lightsOn();
+	Serial.print("this is the x value:  ");Serial.println(pixy->getX());
+	delay(500);
+	Serial.print("this is the y value:  ");Serial.println(pixy->getY());
+	delay(500);
+	Serial.print("this will display if in front:  ");Serial.println(pixy->isInFront());
+	delay(500);
+	Serial.print("this is the area value:  ");Serial.println(pixy->getArea());
+	delay(500);
+	pixy->lightsOff();
+	delay(1000);
+	
+}
+
+

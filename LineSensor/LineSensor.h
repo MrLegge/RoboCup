33132a50335caa057b445114036da0cb6c4f0e76
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2023-08-06 11:10:44 +1000

    Add files via upload

---
+++ LineSensor/LineSensor.h
@@ -0,0 +1,29 @@
+/*
+  LineSensor.h class library to provide line sensing
+  
+  Author: Kyla Bradford y11  12/11/22
+  Adapted: Phil Legge  6/8/23
+  email: phil.legge@det.nsw.edu.au
+*/
+
+#ifndef ROBOTLINESENSOR_H
+#define ROBOTLINESENSOR_H
+
+
+#include <arduino.h>
+       class LineSensor{
+
+          public:
+            LineSensor();
+			LineSensor(int);
+            
+            bool onLine();
+          //  bool reverse();
+
+          protected:
+            bool _lineFlag;
+            int _signalPin;
+           
+      };
+    
+#endif

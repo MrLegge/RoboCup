6a17b5bc7500b47f82b8cee9d1e342499e8e0faa
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2023-08-06 11:18:15 +1000

    Create lineSensor.ino

---
+++ LineSensor/lineSensor.ino
@@ -0,0 +1,28 @@
+/*
+ LineSensor.ino - Demonstration of how to use the LineSensor
+  
+  Author: Phil Legge 12/11/18
+  email: phil.legge@det.nsw.edu.au
+*/
+
+#include "LineSensor.h"
+
+LineSensor *mySensor;
+
+void setup() {
+   mySensor = new LineSensor();
+  //mySensor = new LineSensor(2);
+}
+
+void loop() {
+
+  if(mySensor->onLine()){
+    delay(10);
+    Serial.println("On the line"); 
+    delay(1000);
+  }else{
+    Serial.println("Not on a line"); 
+    delay(1000);
+  }
+  
+}

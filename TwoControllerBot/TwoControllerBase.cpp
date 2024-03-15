1d66d91c1b39069d01e7388fe220b91216c62fad
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2024-03-15 17:12:21 +1100

    Add files via upload

---
+++ TwoControllerBot/TwoControllerBase.cpp
@@ -0,0 +1,98 @@
+/*******************************************************************
+*	This is the workings of the base of your robot, you should not  
+*	need to adapt any of this code unless you want to change the  
+*	pins for the motor controller.
+*
+*	You should use this file to check your pin location against the  
+*	pin values.
+*	
+*	Author: Phil Legge
+*	Date: 7/8/18
+*********************************************************************/
+
+
+#include "TwoControllerBase.h"
+
+  TwoControllerBase::TwoControllerBase(){
+      // signal pins need to be pwm pins on the UNO board (~ near the number)
+      rightFrontSignal = 13; //ENA F
+	  rightRearSignal = 11;  //ENA R
+      leftFrontSignal = 12; //ENB F
+	  leftRearSignal = 10;  //ENB R
+      // these pins don't need to be pwm
+      rightFrontIn1 = 31;
+      rightFrontIn2 = 33;
+      leftFrontIn1 = 35;
+      leftFrontIn2 = 37;
+	  rightRearIn1 = 22;
+      rightRearIn2 = 24;
+      leftRearIn1 = 26;
+      leftRearIn2 = 28;
+      _motorFrontRight = new L298N(rightFrontSignal,rightFrontIn1,rightFrontIn2);
+      _motorFrontLeft = new L298N(leftFrontSignal,leftFrontIn1,leftFrontIn2);
+	  _motorRearRight = new L298N(rightRearSignal,rightRearIn1,rightRearIn2);
+      _motorRearLeft = new L298N(leftRearSignal,leftRearIn1,leftRearIn2);
+    }
+  void TwoControllerBase::driveForward(unsigned short driveSpeed){
+      _motorFrontRight->setSpeed(driveSpeed);
+      _motorFrontLeft->setSpeed(driveSpeed);
+	   _motorRearRight->setSpeed(driveSpeed);
+      _motorRearLeft->setSpeed(driveSpeed);
+      _motorFrontRight->backward();
+      _motorFrontLeft->forward();
+	  _motorRearRight->backward();
+      _motorRearLeft->forward();
+    }
+  void TwoControllerBase::driveBackward(unsigned short driveSpeed){
+       _motorFrontRight->setSpeed(driveSpeed);
+		_motorFrontLeft->setSpeed(driveSpeed);
+	   _motorRearRight->setSpeed(driveSpeed);
+      _motorRearLeft->setSpeed(driveSpeed);
+      _motorFrontRight->forward();
+      _motorFrontLeft->backward();
+	  _motorRearRight->forward();
+      _motorRearLeft->backward();
+    
+    }
+	void TwoControllerBase::driveRight(unsigned short driveSpeed){
+		_motorFrontRight->setSpeed(driveSpeed);
+		_motorFrontLeft->setSpeed(driveSpeed);
+		_motorRearRight->setSpeed(driveSpeed);
+		_motorRearLeft->setSpeed(driveSpeed);
+		_motorFrontRight->backward();
+		_motorFrontLeft->forward();
+		_motorRearRight->forward();
+		_motorRearLeft->backward();
+	}
+	void TwoControllerBase::driveLeft(unsigned short driveSpeed){
+		_motorFrontRight->setSpeed(driveSpeed);
+		_motorFrontLeft->setSpeed(driveSpeed);
+		_motorRearRight->setSpeed(driveSpeed);
+		_motorRearLeft->setSpeed(driveSpeed);
+		_motorFrontRight->forward();
+		_motorFrontLeft->backward();
+		_motorRearRight->backward();
+		_motorRearLeft->forward();	  
+	}
+  void TwoControllerBase::rotateRight(unsigned short turnSpeed){
+		_motorFrontRight->setSpeed(turnSpeed);
+		_motorFrontLeft->setSpeed(turnSpeed);
+		_motorRearRight->setSpeed(turnSpeed);
+		_motorRearLeft->setSpeed(turnSpeed);
+		_motorFrontRight->forward();
+		_motorFrontLeft->forward();
+		_motorRearRight->forward();
+		_motorRearLeft->forward();	
+    
+    }
+  void TwoControllerBase::rotateLeft(unsigned short turnSpeed){
+		_motorFrontRight->setSpeed(turnSpeed);
+		_motorFrontLeft->setSpeed(turnSpeed);
+		_motorRearRight->setSpeed(turnSpeed);
+		_motorRearLeft->setSpeed(turnSpeed);
+		_motorFrontRight->backward();
+		_motorFrontLeft->backward();
+		_motorRearRight->backward();
+		_motorRearLeft->backward();
+    
+    }
---
+++ TwoControllerBot/TwoControllerBase.h
@@ -0,0 +1,43 @@
+/*******************************************************************
+*	This is the library of the base of your robot, you should not  
+*	need to adapt any of this code unless you want to change the  
+*	default constant values for the motor controller.
+*
+*	You should use this file to check the functions available  
+*	for you to use in your setup and loop functions.
+*	
+*	Author: Phil Legge
+*	Date: 7/8/18
+*********************************************************************/
+
+#ifndef TWOCONTROLLERBASE_H
+#define TWOCONTROLLERBASE_H
+
+#include <L298N.h>
+
+	class TwoControllerBase{
+		public:
+		  const int MAX_SPEED = 255; 
+		  const int THREE_QUARTER_SPEED = 138; 
+		  const int HALF_SPEED = 92; 
+		  const int QUARTER_SPEED = 46;  
+		  const int MIN_SPEED = 15; 
+		  int rightFrontSignal, rightFrontIn1, rightFrontIn2, rightRearSignal, rightRearIn1, rightRearIn2; 
+		  int leftFrontSignal, leftFrontIn1, leftFrontIn2, leftRearSignal, leftRearIn1, leftRearIn2; 
+		  
+		  TwoControllerBase();
+		  
+		  void driveForward(unsigned short);
+		  void driveBackward(unsigned short);
+		  void driveRight(unsigned short);
+		  void driveLeft(unsigned short);
+		  void rotateRight(unsigned short);
+		  void rotateLeft(unsigned short);
+		private:
+			L298N *_motorFrontRight;
+			L298N *_motorFrontLeft;
+			L298N *_motorRearRight;
+			L298N *_motorRearLeft;
+	};
+
+#endif 

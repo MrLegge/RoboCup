19b480e1a98eaea32c9900a9dce101dd03ee6d1e
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2024-03-22 10:54:06 +1100

    Update UltraSonicSensor.cpp

--- UltraSonicSensor/UltraSonicSensor.cpp
+++ UltraSonicSensor/UltraSonicSensor.cpp
@@ -12,25 +12,25 @@ UltraSonicSensor::UltraSonicSensor(uint8_t trig,uint8_t echo){
 	_trigPin = trig;
 	_echoPin = echo;
 	pinMode(_trigPin, OUTPUT);
-  pinMode(_echoPin, INPUT);
+  	pinMode(_echoPin, INPUT);
 	
 }
 
 int UltraSonicSensor::getDistance(){
 	digitalWrite(_trigPin, LOW); 
-    delayMicroseconds(2); 
-    digitalWrite(_trigPin, HIGH);
-    delayMicroseconds(10); 
-    digitalWrite(_trigPin, LOW);
-    _duration = pulseIn(_echoPin, HIGH);
-    _distance = (_duration/2) / 29;
+    	delayMicroseconds(2); 
+    	digitalWrite(_trigPin, HIGH);
+    	delayMicroseconds(10); 
+    	digitalWrite(_trigPin, LOW);
+    	_duration = pulseIn(_echoPin, HIGH);
+   	 _distance = (_duration/2) / 29;
     if (_distance >= 300 ){ // return -1 if bigger than 3m
-      return -1;
+     	 return -1;
     }
     else if(_distance <= 0){ // returns 0 for error or zero results
-      return 0;
+      	return 0;
     }
     else {
-      return _distance; // returns a value if test is between 0m and 3m
+      	return _distance; // returns a value if test is between 0m and 3m
     }
 }
